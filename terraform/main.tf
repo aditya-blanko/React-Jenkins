25016b2676526297ab836979ab0db3d3fe867894
Aditya singhal <133752891+aditya-blanko@users.noreply.github.com>
2025-04-06 23:53:39 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,9 +14,8 @@ resource "azurerm_app_service_plan" "asp" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Linux"  
-  reserved            = true 
-      
+  kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Standard"
@@ -24,14 +23,20 @@ resource "azurerm_app_service_plan" "asp" {
   }
 }
 
-# App Service
-resource "azurerm_app_service" "app" {
+# Linux Web App (App Service)
+resource "azurerm_linux_web_app" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  service_plan_id     = azurerm_app_service_plan.asp.id
+
   site_config {
-    linux_fx_version = "NODE|18-lts" 
+    linux_fx_version = "NODE|18-lts"
+    always_on        = true
   }
-  
+
+  app_settings = {
+    WEBSITE_RUN_FROM_PACKAGE = "1"  # Critical for ZIP deployment
+    NODE_ENV                 = "production"
+  }
 }
