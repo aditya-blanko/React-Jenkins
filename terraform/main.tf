0e05b375a7d5e6ddf98b4e59100e9f2aec119731
Aditya singhal <133752891+aditya-blanko@users.noreply.github.com>
2025-04-07 10:45:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,13 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "bf9c4a23-fb6a-43a2-a6c4-9e224b69b5ac"
-}
-
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
+# App Service Plan (Windows)
 resource "azurerm_service_plan" "asp" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Windows"  # Windows OS
+  os_type             = "Windows"  # Required for Windows
   sku_name            = "S1"       # Standard pricing tier
 }
 
-
+# Windows Web App (Node.js 18)
 resource "azurerm_windows_web_app" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
@@ -27,13 +17,13 @@ resource "azurerm_windows_web_app" "app" {
   site_config {
     always_on = true
     application_stack {
-      node_version = "18-lts"  # Specify the Node.js runtime version
+      node_version = "~18"  # Use tilde (~) syntax instead of "-lts"
     }
   }
 
   app_settings = {
     WEBSITE_RUN_FROM_PACKAGE     = "1"
     NODE_ENV                     = "production"
-    WEBSITE_NODE_DEFAULT_VERSION = "~18" # Use the latest Node.js 18 runtime
+    WEBSITE_NODE_DEFAULT_VERSION = "~18"  # Match node_version
   }
 }
